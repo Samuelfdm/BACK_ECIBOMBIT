3260f77c2190d5bd5aa4431bd90709a6d7d5d837
Samuel Felipe Diaz <samuelfdm14@gmail.com>
2025-05-12 16:33:36 -0500

    ADD: Deploy35

--- src/test/java/edu/eci/arsw/ecibombit/EciBombitApplicationTests.java
+++ src/test/java/edu/eci/arsw/ecibombit/EciBombitApplicationTests.java
@@ -3,11 +3,9 @@ package edu.eci.arsw.ecibombit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.cloud.azure.keyvault.secret.property-sources[0].enabled=false"})
 class EciBombitApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
